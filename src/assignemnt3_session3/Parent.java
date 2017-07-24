e4772dce42d59ef2dec1a7838921bb49dbf25ae5
AnilKumar2087 <a.anil.adunuri@gmail.com>
2017-07-24 23:18:28 +0530

    changes

---
+++ src/assignemnt3_session3/Parent.java
@@ -0,0 +1,17 @@
+package assignemnt3_session3;
+
+class Parent{
+    private String name;
+    /*
+     * Calling constructor of same class with one String argument
+     */
+    protected Parent(){
+        this("");
+        System.out.println("No argument constructor of Parent called ");
+    }
+  
+    protected Parent(String name){
+        this.name = name;
+        System.out.println("One String argument constructor of Parent called ");
+    }
+}
